37b22cf62d74ec90696bb8a79684096bd8c39be8
k0rm <kormakur20@ru.is>
2022-03-20 10:49:58 +0000

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Carousel } from "./components";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel
+        images={[
+          "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/6606.png&w=350&h=254",
+          "https://dotorg.brightspotcdn.com/dims4/default/05583ce/2147483647/strip/true/crop/800x800+0+0/resize/1200x1200!/quality/90/?url=http%3A%2F%2Fsoi-brightspot.s3.amazonaws.com%2Fdotorg%2F8b%2Fea%2F46d1425f493aa39511659f5cc12d%2F800x800-damian-lillard.jpg",
+          "https://cloudfront-us-east-1.images.arcpublishing.com/opb/GM36AKBAXFA5ZOQ4LOLTXOGIAU.jpg"
+        ]}
+        size={"medium"}
+      />
     </div>
   );
 }
---
+++ src/components/Carousel/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styles from './carousel.module.css';
+
+const Carousel = ({ images, size }) => {
+    
+    let len = images.length - 1;
+    const [index, setIndex] = useState(0);
+    const [image, setImage] = useState(images[index]);
+
+
+    const carouselToggler = (direction) => {
+        direction
+            ?
+        index === len ? setIndex(0) : setIndex(index + 1)
+            :
+        index === 0 ? setIndex(len) : setIndex(index - 1)
+
+        setImage(images[index]);
+    }
+
+    return (
+        <div className={ styles.carousel }>
+            <button className={ styles[size] } onClick={() => carouselToggler(false)}>
+                -
+            </button>
+            <div className={ styles.carouselImageContainer }>
+                <img src={image} alt="car imgoo" className={ styles[size] } />
+            </div>
+            <button className={ styles[size] } onClick={() => carouselToggler(true)}>
+                +
+            </button>
+        </div>
+    )
+}
+
+Carousel.propTypes = {
+    images: PropTypes.arrayOf(PropTypes.string),
+    size: PropTypes.oneOf([ "small", "medium", "large"])
+}
+
+
+export default Carousel;
